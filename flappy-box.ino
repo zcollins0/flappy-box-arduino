23907ef649273274a0766cb611db78ef790de805
Zach Collins <collinzy@mail.uc.edu>
2018-04-07 23:27:52 -0400

    replace incorrect header comment

--- flappy-box.ino
+++ flappy-box.ino
@@ -1,23 +1,5 @@
-/*************************************************** 
-  This is a library for our I2C LED Backpacks
-
-  Designed specifically to work with the Adafruit LED Matrix backpacks 
-  ----> http://www.adafruit.com/products/872
-  ----> http://www.adafruit.com/products/871
-  ----> http://www.adafruit.com/products/870
-
-  These displays use I2C to communicate, 2 pins are required to 
-  interface. There are multiple selectable I2C addresses. For backpacks
-  with 2 Address Select pins: 0x70, 0x71, 0x72 or 0x73. For backpacks
-  with 3 Address Select pins: 0x70 thru 0x77
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+/* Flappy box for Arduino.
+ * by Zach Collins*/
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -145,8 +127,6 @@ void button_ISR() {
     updateCounter = 255;
   }
 }
-
-
 
 
 
